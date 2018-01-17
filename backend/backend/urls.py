7a21eb072346af54f8bd05653979bd8c6608db15
mayne <dreama9ain@gmail.com>
2018-01-17 10:50:30 +0800

    fix

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework import routers
-from ..api import views
+from api import views
 
 router = routers.DefaultRouter()
 
