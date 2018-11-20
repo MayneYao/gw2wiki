4e61e37520a6b9e283d1f96d56c33b6ef316c07f
mayne <dreama9ain@gmail.com>
2018-11-20 17:39:42 +0800

    添加测试组件

---
+++ huiji_component/build/precache-manifest.19a539721187b4886ba5c00d827443d1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d12d19a6b64c31ef3a82",
+    "url": "/static/js/main.d12d19a6.chunk.js"
+  },
+  {
+    "revision": "7eaba8bc086576cde7b0",
+    "url": "/static/js/1.7eaba8bc.chunk.js"
+  },
+  {
+    "revision": "faa3910393f0964e02f756af3a8ce13e",
+    "url": "/index.html"
+  }
+];
